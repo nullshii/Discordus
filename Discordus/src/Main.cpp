d17520da557f11531aaa56b787e94a0b373542da
Ray <icefoxtx@gmail.com>
2022-06-27 02:04:10 +0400

    Added app bar & debug menu

--- Discordus/src/Main.cpp
+++ Discordus/src/Main.cpp
@@ -1,22 +1,199 @@
 #include <DiscordRPC.h>
 #include <memory.h>
-#include <iostream>
 #include <time.h>
+#include <stdio.h>
+#include <iostream>
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <imgui.h>
+#include <backends/imgui_impl_glfw.h>
+#include <backends/imgui_impl_opengl3.h>
 
-int main()
+static bool debugMenu = true;
+GLFWwindow *appWindow;
+
+void quit()
 {
+    glfwSetWindowShouldClose(appWindow, true);
+}
+
+void saveToFile()
+{
+}
+
+void openFile()
+{
+}
+
+void RenderAppBar()
+{
+    {
+        ImGui::BeginMainMenuBar();
+
+        if (ImGui::BeginMenu("App"))
+        {
+            if (ImGui::MenuItem("Quit", "Ctrl + Q"))
+            {
+                quit();
+            }
+
+            ImGui::EndMenu();
+        }
+
+        if (ImGui::BeginMenu("File"))
+        {
+            if (ImGui::MenuItem("Save", "Ctrl + S"))
+            {
+                saveToFile();
+            }
+
+            if (ImGui::MenuItem("Open", "Ctrl + O"))
+            {
+                openFile();
+            }
+
+            ImGui::EndMenu();
+        }
+
+        ImGui::EndMainMenuBar();
+    }
+}
+
+void RenderApp()
+{
+    if (debugMenu)
+    {
+        ImGui::Begin("Debug menu", &debugMenu);
+        ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
+        ImGui::End();
+    }
+}
+
+static void glfw_error_callback(int error, const char *description)
+{
+    std::cout << "Glfw Error " << error << ": " << description << std::endl;
+}
+
+void keyCallback(GLFWwindow *window, int key, int scancode, int action, int mods)
+{
+    if (action == GLFW_PRESS && mods == GLFW_MOD_CONTROL && key == GLFW_KEY_Q)
+    {
+        quit();
+    }
+    else if (action == GLFW_PRESS && mods == GLFW_MOD_CONTROL && key == GLFW_KEY_S)
+    {
+        saveToFile();
+    }
+    else if (action == GLFW_PRESS && mods == GLFW_MOD_CONTROL && key == GLFW_KEY_O)
+    {
+        openFile();
+    }
+}
+
+int main(int, char **)
+{
+    glfwSetErrorCallback(glfw_error_callback);
+    if (!glfwInit())
+        return 1;
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
+
+    appWindow = glfwCreateWindow(1280, 720, "Discordus", nullptr, nullptr);
+    if (!appWindow)
+        return 1;
+
+    glfwMakeContextCurrent(appWindow);
+    glfwSwapInterval(0);
+
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+        return 1;
+
+    glfwSetKeyCallback(appWindow, keyCallback);
+
+    std::cout << "Vendor: " << glGetString(GL_VENDOR) << std::endl;
+    std::cout << "Version: " << glGetString(GL_VERSION) << std::endl;
+
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+    ImGuiIO &io = ImGui::GetIO();
+    io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;
+    io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;
+    io.ConfigFlags |= ImGuiConfigFlags_ViewportsEnable;
+    // io.ConfigViewportsNoAutoMerge = true;
+    // io.ConfigViewportsNoTaskBarIcon = true;
+
+    ImGui::StyleColorsDark();
+
+    ImGuiStyle &style = ImGui::GetStyle();
+    if (io.ConfigFlags & ImGuiConfigFlags_ViewportsEnable)
+    {
+        style.WindowRounding = 0.0f;
+        style.Colors[ImGuiCol_WindowBg].w = 1.0f;
+    }
+
+    ImGui_ImplGlfw_InitForOpenGL(appWindow, true);
+    ImGui_ImplOpenGL3_Init("#version 130");
+
+    // Load Fonts
+    // - If no fonts are loaded, dear imgui will use the default font. You can also load multiple fonts and use ImGui::PushFont()/PopFont() to select them.
+    // - AddFontFromFileTTF() will return the ImFont* so you can store it if you need to select the font among multiple.
+    // - If the file cannot be loaded, the function will return NULL. Please handle those errors in your application (e.g. use an assertion, or display an error and quit).
+    // - The fonts will be rasterized at a given size (w/ oversampling) and stored into a texture when calling ImFontAtlas::Build()/GetTexDataAsXXXX(), which ImGui_ImplXXXX_NewFrame below will call.
+    // - Read 'docs/FONTS.md' for more instructions and details.
+    // - Remember that in C/C++ if you want to include a backslash \ in a string literal you need to write a double backslash \\ !
+    // io.Fonts->AddFontDefault();
+    // io.Fonts->AddFontFromFileTTF("../../misc/fonts/Roboto-Medium.ttf", 16.0f);
+    // io.Fonts->AddFontFromFileTTF("../../misc/fonts/Cousine-Regular.ttf", 15.0f);
+    // io.Fonts->AddFontFromFileTTF("../../misc/fonts/DroidSans.ttf", 16.0f);
+    // io.Fonts->AddFontFromFileTTF("../../misc/fonts/ProggyTiny.ttf", 10.0f);
+    // ImFont* font = io.Fonts->AddFontFromFileTTF("c:\\Windows\\Fonts\\ArialUni.ttf", 18.0f, NULL, io.Fonts->GetGlyphRangesJapanese());
+    // IM_ASSERT(font != NULL);
+
     DiscordRPC *rpc = new DiscordRPC("990661105124786196");
 
     rpc->SetLargeImageKey("discordus");
     rpc->SetLargeImageText("discordus");
     rpc->UpdatePresence();
 
-    while (true)
+    while (!glfwWindowShouldClose(appWindow))
     {
-        std::cin.get();
-        break;
+        glfwPollEvents();
+
+        ImGui_ImplOpenGL3_NewFrame();
+        ImGui_ImplGlfw_NewFrame();
+        ImGui::NewFrame();
+
+        RenderAppBar();
+        RenderApp();
+
+        ImGui::Render();
+        int display_w, display_h;
+        glfwGetFramebufferSize(appWindow, &display_w, &display_h);
+        glViewport(0, 0, display_w, display_h);
+        glClearColor(0.2f, 0.2f, 0.2f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+
+        if (io.ConfigFlags & ImGuiConfigFlags_ViewportsEnable)
+        {
+            GLFWwindow *backup_current_context = glfwGetCurrentContext();
+            ImGui::UpdatePlatformWindows();
+            ImGui::RenderPlatformWindowsDefault();
+            glfwMakeContextCurrent(backup_current_context);
+        }
+
+        glfwSwapBuffers(appWindow);
     }
 
+    ImGui_ImplOpenGL3_Shutdown();
+    ImGui_ImplGlfw_Shutdown();
+    ImGui::DestroyContext();
+
     delete rpc;
+
+    glfwDestroyWindow(appWindow);
+    glfwTerminate();
+
     return 0;
 }
