729b2841db837d6523c890e9fdf32160ebf09e91
Ray <icefoxtx@gmail.com>
2022-06-26 22:18:58 +0400

    Testing DiscordRPCpp

--- Discordus/src/Main.cpp
+++ Discordus/src/Main.cpp
@@ -1,4 +1,22 @@
+#include <DiscordRPC.h>
+#include <memory.h>
+#include <iostream>
+#include <time.h>
+
 int main()
 {
+    DiscordRPC *rpc = new DiscordRPC("990661105124786196");
+
+    rpc->SetLargeImageKey("discordus");
+    rpc->SetLargeImageText("discordus");
+    rpc->UpdatePresence();
+
+    while (true)
+    {
+        std::cin.get();
+        break;
+    }
+
+    delete rpc;
     return 0;
 }
