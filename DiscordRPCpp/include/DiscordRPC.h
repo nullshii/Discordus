94124eb40f65587020afc43e5d295e752ccff79c
Ray <icefoxtx@gmail.com>
2022-06-27 18:39:54 +0400

    Added GetPresence() and formatted code

--- DiscordRPCpp/include/DiscordRPC.h
+++ DiscordRPCpp/include/DiscordRPC.h
@@ -4,41 +4,43 @@
 
 class DiscordRPC
 {
-public:
-    DiscordRPC(const char *appId);
-    ~DiscordRPC();
+  public:
+	DiscordRPC(const char *appId);
+	~DiscordRPC();
 
-    void SetPresence(DiscordRichPresence &presence);
-    void UpdatePresence();
-    void ClearPresence();
+	inline void SetPresence(DiscordRichPresence &presence) { m_Presence = presence; }
+	inline const DiscordRichPresence GetPresence() const { return m_Presence; }
 
-    void SetInstance(int &instance);
-    void SetState(const char *state);
-    void SetDetails(const char *details);
-    void SetStartTimestamp(int64_t &startTimeStamp);
-    void SetEndTimestamp(int64_t &endTimeStamp);
-    void SetJoinSecret(const char *joinSecret);
-    void SetMatchSecret(const char *matchSecret);
-    void SetSpectateSecret(const char *spectateSecret);
-    void SetLargeImageKey(const char *largeImageKey);
-    void SetLargeImageText(const char *largeImageText);
-    void SetSmallImageKey(const char *smallImageKey);
-    void SetSmallImageText(const char *smallImageText);
-    void SetPartyId(const char *partyId);
-    void SetPartySize(int &partySize);
-    void SetPartyMax(int &partyMax);
-    void SetPartyPrivacy(int &partyPrivacy);
+	void UpdatePresence();
+	void ClearPresence();
 
-private:
-    static void handleDiscordReady(const DiscordUser *connectedUser);
-    static void handleDiscordDisconnected(int errcode, const char *message);
-    static void handleDiscordError(int errcode, const char *message);
-    static void handleDiscordJoin(const char *secret);
-    static void handleDiscordSpectate(const char *secret);
-    static void handleDiscordJoinRequest(const DiscordUser *request);
-    static void internalUpdate();
+	void SetInstance(int &instance);
+	void SetState(const char *state);
+	void SetDetails(const char *details);
+	void SetStartTimestamp(int64_t &startTimeStamp);
+	void SetEndTimestamp(int64_t &endTimeStamp);
+	void SetJoinSecret(const char *joinSecret);
+	void SetMatchSecret(const char *matchSecret);
+	void SetSpectateSecret(const char *spectateSecret);
+	void SetLargeImageKey(const char *largeImageKey);
+	void SetLargeImageText(const char *largeImageText);
+	void SetSmallImageKey(const char *smallImageKey);
+	void SetSmallImageText(const char *smallImageText);
+	void SetPartyId(const char *partyId);
+	void SetPartySize(int &partySize);
+	void SetPartyMax(int &partyMax);
+	void SetPartyPrivacy(int &partyPrivacy);
 
-private:
-    DiscordRichPresence m_Presence;
-    DiscordEventHandlers m_Handlers;
+  private:
+	static void handleDiscordReady(const DiscordUser *connectedUser);
+	static void handleDiscordDisconnected(int errcode, const char *message);
+	static void handleDiscordError(int errcode, const char *message);
+	static void handleDiscordJoin(const char *secret);
+	static void handleDiscordSpectate(const char *secret);
+	static void handleDiscordJoinRequest(const DiscordUser *request);
+	static void internalUpdate();
+
+  private:
+	DiscordRichPresence m_Presence;
+	DiscordEventHandlers m_Handlers;
 };
