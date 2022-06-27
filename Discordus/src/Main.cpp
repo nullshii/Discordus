b6bbfd3a7b2272f49eb524cf536c12082fd0c798
Ray <icefoxtx@gmail.com>
2022-06-27 18:47:48 +0400

    Removed unnecessary code

--- Discordus/src/Main.cpp
+++ Discordus/src/Main.cpp
@@ -369,9 +369,6 @@ int main(int, char **)
 		RenderApp();
 
 		ImGui::Render();
-		int display_w, display_h;
-		glfwGetFramebufferSize(appWindow, &display_w, &display_h);
-		glViewport(0, 0, display_w, display_h);
 		glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
 		ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
