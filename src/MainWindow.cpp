8383d17eae3b17b458e1df6b1d673f1cc1ed05c1
Ray <60722967+PureFoxCore@users.noreply.github.com>
2022-08-22 15:00:01 +0400

    Added buttons and serialization

--- src/MainWindow.cpp
+++ src/MainWindow.cpp
@@ -1,3 +1,48 @@
-#include "MainWindow.h"
+#include "MainWindow.hpp"
 
-MainWindow::MainWindow() : wxFrame(NULL, wxID_ANY, "Discordus") {}
+#include "FileSerializer.hpp"
+#include "PresenceData.hpp"
+
+#include <cstring>
+
+MainWindow::MainWindow() : wxFrame(NULL, wxID_ANY, "Discordus") {
+  this->SetMinSize(wxSize(800, 500));
+
+  wxMenu *menuApp = new wxMenu();
+  menuApp->Append(wxID_EXIT);
+
+  wxMenu *menuFile = new wxMenu();
+  menuFile->Append(wxID_SAVE);
+  menuFile->Append(wxID_OPEN);
+
+  wxMenu *menuHelp = new wxMenu();
+  menuHelp->Append(wxID_ABOUT);
+
+  wxMenuBar *menuBar = new wxMenuBar();
+  menuBar->Append(menuApp, "&App");
+  menuBar->Append(menuFile, "&File");
+  menuBar->Append(menuHelp, "&Help");
+
+  m_Text = new wxStaticText(this, wxID_ANY, "AAAAAAAAAAAAAA");
+
+  this->SetMenuBar(menuBar);
+
+  Bind(wxEVT_MENU, &MainWindow::OnSave, this, wxID_SAVE);
+  Bind(wxEVT_MENU, &MainWindow::OnOpen, this, wxID_OPEN);
+  Bind(wxEVT_MENU, &MainWindow::OnExit, this, wxID_EXIT);
+}
+
+void MainWindow::OnSave(wxCommandEvent &event) {
+  const char *texts = "Some text";
+  PresenceData data;
+  data.number = 52;
+  FileSerializer::Serialize<PresenceData>(&data, "File.sav");
+}
+
+void MainWindow::OnOpen(wxCommandEvent &event) {
+  PresenceData data;
+  FileSerializer::Deserialize<PresenceData>(&data, "File.sav");
+  m_Text->SetLabel(std::to_string(data.number) + " " + data.text);
+}
+
+void MainWindow::OnExit(wxCommandEvent &event) { Close(true); }
